33bbcb70c9b4c96b900f8090b909e869d56b4c37
shakil4611 <shakilbreakingchemistry@gmail.com>
2023-04-05 21:50:21 +0600

    break and condition

---
+++ break.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    for ( i = 1; i <= 100; i=i+1)
+    {
+    
+      
+        printf("%d\n",i);
+              if (i==50)
+        {
+             break;
+        }
+    }
+    
+    return 0;
+}
